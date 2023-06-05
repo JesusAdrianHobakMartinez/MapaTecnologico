b35aca42a9a1c34997a9dbf6e9bbc6151ff3b9b6
JesusAdrianHobakMartinez <jesus.18070037@itsmotul.edu.mx>
2023-06-04 23:08:45 -0600

    creacion del componente en mapa-menu

--- resources/views/livewire/menu/mapa-menu-principal.blade.php
+++ resources/views/livewire/menu/mapa-menu-principal.blade.php
@@ -8,7 +8,7 @@
 						<div class="float-left">
 							<h4>Mapa de la escuela</h4>
 						</div>
-                        <a class="btn btn-info" wire:click="$emit('refreshComponent')" href=""></i>Mapa completo</a>
+                        <a class="btn btn-warning" wire:click="$emit('refreshComponent')" href=""></i>Mapa completo</a>
 
 						@if (session()->has('message'))
 						<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
