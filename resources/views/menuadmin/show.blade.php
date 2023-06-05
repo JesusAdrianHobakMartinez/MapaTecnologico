b264a662a636c5ad679b252e51b52bdd221edfcc
JesusAdrianHobakMartinez <jesus.18070037@itsmotul.edu.mx>
2023-06-04 23:13:17 -0600

    creacion del card que mostrar el los detalles del departamento

--- resources/views/menuadmin/show.blade.php
+++ resources/views/menuadmin/show.blade.php
@@ -5,7 +5,7 @@
       </button></a>
     <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
 
-        <h2 class="mx-auto">Detalles departamento</h2>
+        <h2 class="mx-auto">Departamento</h2>
         <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" >
             Nombre departamento:
